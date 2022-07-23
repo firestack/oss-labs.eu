1a216ad3fdbecef7fd857cfa604eea51ee1e2b97
Pol Dellaiera <pol.dellaiera@protonmail.com>
2022-07-23 09:10:13 +0200

    Update.

--- flake.nix
+++ flake.nix
@@ -3,70 +3,70 @@
 
   inputs = {
     nixpkgs.url = "github:/nixos/nixpkgs/nixos-unstable";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
+    flake-utils.url = "github:numtide/flake-utils";
+    deploy-rs.url = "github:serokell/deploy-rs";
+    deploy-rs.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    plasma-manager.url = "github:pjones/plasma-manager";
-    deploy-rs.url = "github:serokell/deploy-rs";
 
     # Fish theme
     bobthefish = { url = "github:oh-my-fish/theme-bobthefish"; flake = false; };
   };
 
   outputs = { self, nixpkgs, nixos-hardware, home-manager, nixpkgs-unstable, deploy-rs, ... }@inputs:
-    let
-      overlay-unstable = final: prev: {
-        unstable = import inputs.nixpkgs-unstable {
-          inherit (final) config;
+    {
+      nixosConfigurations = {
+        hedgedoc = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
+          modules = [
+            ({
+              nixpkgs.overlays = [
+                (final: prev: { bobthefish-src = inputs.bobthefish; })
+              ];
+            })
+            (import ./hosts/pad.oss-labs.eu/packages.nix)
+            (import ./hosts/pad.oss-labs.eu/configuration.nix)
+            (import ./hosts/pad.oss-labs.eu/postgres.nix)
+            (import ./hosts/pad.oss-labs.eu/hedgedoc.nix)
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.root.imports = [
+                ./hosts/common/home.nix
+                { home.stateVersion = "22.05"; }
+              ];
+            }
+
+          ];
+          specialArgs = inputs;
         };
       };
-    in
-    {
-    nixosConfigurations = {
-      hedgedoc = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        # disabledModules = [ "services/web-apps/hedgedoc.nix" ];
-        modules = [
-          ({ nixpkgs.overlays = [
-             (final: prev: { bobthefish-src = inputs.bobthefish; })
-             overlay-unstable
-          ]; })
-          (import ./hosts/pad.oss-labs.eu/packages.nix)
-          (import ./hosts/pad.oss-labs.eu/configuration.nix)
-          (import ./hosts/pad.oss-labs.eu/postgres.nix)
-          (import ./hosts/pad.oss-labs.eu/hedgedoc.nix)
-        ];
-        specialArgs = inputs;
-      };
-    };
-  } // {
-      deploy.nodes.oss-labs = {
-        hostname = "ec2-34-242-86-57.eu-west-1.compute.amazonaws.com";
+    } // {
+      deploy.nodes.hedgedoc = {
+        hostname = "ec2-18-202-250-170.eu-west-1.compute.amazonaws.com";
         fastConnection = true;
         profiles = {
-          hedgedoc = {
+          system = {
             sshUser = "root";
-            path =
-              deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.hedgedoc;
+            path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.hedgedoc;
             user = "root";
           };
         };
       };
-  } //inputs.flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = inputs.nixpkgs.legacyPackages.${system};
-    in
-    {
-      devShells = {
-        default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [ nixpkgs-fmt nixfmt ];
+    } // inputs.flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = inputs.nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [ nixpkgs-fmt nixfmt ];
+          };
         };
-      };
-
-    }
-  );
+      }
+    );
 }
 
---
+++ hosts/common/home.nix
@@ -0,0 +1,40 @@
+{ config, pkgs, ... }:
+
+{
+
+  programs = {
+    bat = {
+      enable = true;
+    };
+    direnv = {
+      enable = true;
+      nix-direnv.enable = true;
+    };
+    exa = {
+      enable = true;
+    };
+    fish = {
+      enable = true;
+      interactiveShellInit = ''
+        any-nix-shell fish --info-right | source
+      '';
+      plugins = [
+        {
+          name = "bobthefish";
+          src = pkgs.bobthefish-src;
+        }
+      ];
+      shellAliases = {
+        ll = "exa -lha";
+        cat = "bat";
+        ls = "exa";
+      };
+    };
+    home-manager = {
+      enable = true;
+    };
+    htop = {
+      enable = true;
+    };
+  };
+}
